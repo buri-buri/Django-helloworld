b9cd483feb3748b914ac634042371ffe4b434805
buriburi <sagarshayr999@gmail.com>
2020-08-02 20:51:52 +0530

    save changes for wrong spelling of hwlo world

--- DjangoLearn/myapp/views.py
+++ DjangoLearn/myapp/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    response=HttpResponse('Hello Wrld')
+    response=HttpResponse('Hello World')
     return response
