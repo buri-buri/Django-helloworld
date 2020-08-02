aa9d1652717d9cdd9e8a0337daf003399f724003
buriburi <sagarshayr999@gmail.com>
2020-08-02 23:09:10 +0530

    learned how to use pyhton code in html using echo tag  i.e, passing dictionary to index.html

--- DjangoLearn/myapp/views.py
+++ DjangoLearn/myapp/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    response=render(request,'index.html')
+    developer='sagar kaushik'
+    family=['sagar','shivam','varsha','vinita']
+    #family=['hot girls','smart girls','coder girls']
+    d={'developer':developer,'family':family}
+    response=render(request,'index.html',d)
     return response
