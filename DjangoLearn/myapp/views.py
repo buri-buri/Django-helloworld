e9e99d6deab32276163ccb13478e7877c9d790bd
buriburi <sagarshayr999@gmail.com>
2020-08-02 22:05:32 +0530

    currently i used template file to return response using template instead of string

--- DjangoLearn/myapp/views.py
+++ DjangoLearn/myapp/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    response=HttpResponse('Hello World')
+    response=render(request,'index.html')
     return response
